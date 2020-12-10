f8e3a9f0072a09f7c1cba78c16413a29903c48f1
gitdaestela <estelacriart@gmail.com>
2020-12-10 20:42:34 -0300

    Componente cadastro e login

--- blogPessoal/src/app/cadastro/cadastro.component.ts
+++ blogPessoal/src/app/cadastro/cadastro.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css']
 })
+
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User()
+  senha: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  conferirSenha(event: any){
+    this.senha = event.target.value
+  }
+
+  cadastrar(){
+    if(this.senha === this.user.senha) {
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/login'])
+        alert('Usuário cadastrado com sucesso!!')
+      })
+    } else{
+      alert('As senhas não conferem!')
+    }
   }
 
 }
--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -7,3 +7,5 @@ export class PostagemService {
 
   constructor() { }
 }
+
+/*get all postagens*/
